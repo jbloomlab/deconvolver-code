ed0d0616040ba2b49dd40767c67cc2a07dab2680
naxelrod <naxelrod@eaab159b-83d7-4f8b-918c-c98f9e5e3013>
2010-02-23 21:56:45 +0000

    Removing unnecessary files

--- Grid/t/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-#$ -cwd
-#$ -S /bin/bash
-#$ -o /tmp/$USER.$JOB_ID.$TASK_ID
-# qsub -P 810001 -N SGE_Submit -t 1-3:1  /usr/local/devel/ANNOTATION/naxelrod/lib/Grid/t/cleanup.sh 
-rm -f /tmp/*naxelrod*
-
